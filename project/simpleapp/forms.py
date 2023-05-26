b60766db53a4ebd96c93ca0c355d7779e81ee287
Name <email@example.ru>
2023-05-26 15:42:14 +0300

    online_store

---
+++ project/simpleapp/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+   class Meta:
+       model = Product
+       fields = '__all__'
+
+
+class ProductForm(forms.ModelForm):
+   class Meta:
+       model = Product
+       fields = [
+           'name',
+           'description',
+           'quantity',
+           'category',
+           'price',
+       ]
